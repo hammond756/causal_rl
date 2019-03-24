f91d9515a706f0cac2979fe0262e07d3b6894e26
aronhammond <hammond756@live.nl>
2019-03-24 11:56:07 +0100

    draw function can save plot to path

--- causal_rl/sem/utils.py
+++ causal_rl/sem/utils.py
@@ -1,14 +1,21 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def draw(graph):
+def draw(graph, path=None):
     # transpose because nx edges are (from, to) and DAGs are (to, from)
-    edges = graph.T.nonzero() 
-    edges = zip(*edges)
+    edges = graph.t().nonzero().numpy()
 
+    # initialize graph
     G = nx.DiGraph()
     G.add_edges_from(edges)
 
+    # plot graph
     plt.subplot(111)
     nx.draw(G, with_labels=True)
-    plt.show()
+
+    # save or show based on provided path
+    if path is None:
+        plt.show()
+    else:
+        plt.savefig(path, bbox_inches='tight')
+    
