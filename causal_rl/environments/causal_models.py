ab9d99c8921edff8f1aec0f5a7bc040be99a2acc
aronhammond <hammond756@live.nl>
2019-03-27 10:39:13 +0100

    extra models

--- causal_rl/environments/causal_models.py
+++ causal_rl/environments/causal_models.py
@@ -36,9 +36,19 @@ classic_confounding = torch.tensor([
      [1, 1, 0]]
 ])
 
+linear_mountaincar = torch.tensor([
+    [[1, 1, 0],
+     [1, 1, 0],
+     [0, 0, 0]],
+    [[0, 0, 0],
+     [0, 0, 0],
+     [0, 1, 0]]
+])
+
 causal_models = {
     'chain' : chain,
     'common_cause' : common_cause,
     'common_effect' : common_effect,
-    'classic_confounding' : classic_confounding
+    'classic_confounding' : classic_confounding,
+    'linear_mountaincar' : linear_mountaincar
 }
