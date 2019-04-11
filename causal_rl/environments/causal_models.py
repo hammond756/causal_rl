2713a87031e33c67a4c1e91af96b08a3aa81d715
aronhammond <hammond756@live.nl>
2019-04-11 16:09:00 +0200

    added stacked-chain model

--- causal_rl/environments/causal_models.py
+++ causal_rl/environments/causal_models.py
@@ -54,11 +54,29 @@ independent = torch.tensor([
      [0, 0, 0]]
 ])
 
+stacked_chain = torch.tensor([
+    [[0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0]],
+    [[0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0],
+     [1, 1, 0, 0, 0, 0, 0],
+     [1, 1, 0, 0, 0, 0, 0],
+     [0, 0, 1, 1, 0, 0, 0],
+     [0, 0, 0, 0, 1, 0, 0],
+     [0, 0, 0, 0, 1, 0, 0]]
+])
+
 causal_models = {
     'chain' : chain,
     'common_cause' : common_cause,
     'common_effect' : common_effect,
     'classic_confounding' : classic_confounding,
     'linear_mountaincar' : linear_mountaincar,
-    'independent' : independent
+    'independent' : independent,
+    'stacked_chain' : stacked_chain
 }
