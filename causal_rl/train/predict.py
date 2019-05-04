64bf33afe791c15b0a1580e663c304e04cbf9787
aronhammond <hammond756@live.nl>
2019-05-04 15:59:26 +0200

    moved argparse helpers to train script

--- causal_rl/train/predict.py
+++ causal_rl/train/predict.py
@@ -81,23 +81,6 @@ class Predictor(nn.Module):
 
         return out
 
-def save_configuration(config):
-    """
-    Creates a file in the output directory specified in the _config_ object
-    with all the arguments and values. This file can be used to re-run the
-    script with the same parameters.
-    """
-    with open(config.output_dir + '/config.txt', 'w') as f:
-        for key, value in vars(config).items():
-            f.write('--{}\n'.format(key))
-
-            # if nargs > 1 they all need to be on a new line
-            if type(value) == list:
-                for val in value:
-                    f.write('{}\n'.format(val))
-            else:
-                f.write('{}\n'.format(value))
-        
 def predict(sem, config):
 
     n_iterations = config.n_iters
@@ -268,21 +251,3 @@ def predict(sem, config):
         'action_probs' : action_probs if not config.use_random else None,
         'reward' : reward_log if not config.use_random else None
     }
-
-class readable_dir(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        prospective_dir=values
-        if not os.path.isdir(prospective_dir):
-            raise argparse.ArgumentTypeError("readable_dir:{0} is not a valid path".format(prospective_dir))
-        if os.access(prospective_dir, os.R_OK):
-            setattr(namespace,self.dest,prospective_dir)
-        else:
-            raise argparse.ArgumentTypeError("readable_dir:{0} is not a readable dir".format(prospective_dir))
-
-def str2bool(v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
--- train.py
+++ train.py
@@ -7,6 +7,41 @@ from causal_rl.sem import StructuralEquationModel
 from causal_rl.sem.utils import draw
 from causal_rl.train import predict
 
+def save_configuration(config):
+    """
+    Creates a file in the output directory specified in the _config_ object
+    with all the arguments and values. This file can be used to re-run the
+    script with the same parameters.
+    """
+    with open(config.output_dir + '/config.txt', 'w') as f:
+        for key, value in vars(config).items():
+            f.write('--{}\n'.format(key))
+
+            # if nargs > 1 they all need to be on a new line
+            if type(value) == list:
+                for val in value:
+                    f.write('{}\n'.format(val))
+            else:
+                f.write('{}\n'.format(value))
+
+class readable_dir(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        prospective_dir=values
+        if not os.path.isdir(prospective_dir):
+            raise argparse.ArgumentTypeError("readable_dir:{0} is not a valid path".format(prospective_dir))
+        if os.access(prospective_dir, os.R_OK):
+            setattr(namespace,self.dest,prospective_dir)
+        else:
+            raise argparse.ArgumentTypeError("readable_dir:{0} is not a readable dir".format(prospective_dir))
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
 
