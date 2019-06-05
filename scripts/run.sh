da24b66ac97306f8c5a6bc0d201aedc30feb20a3
aronhammond <hammond756@live.nl>
2019-06-05 13:04:57 +0200

    don't constrain prediction matrix

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,15 +2,15 @@
 
 python train.py \
         --intervention_value 5 \
-        --n_iters 10000 \
+        --n_iters 50000 \
         --use_random True \
         --entr_loss_coeff 0 \
         --log_iters 1000 \
         --lr 0.001 \
         --noise_dist bernoulli 0.5 \
         --predictor two_step \
-        --reg_lambda 0 \
+        --reg_lambda 1 \
         --dag_name random \
-        --random_dag 4 0.6 \
+        --random_dag 3 0.6 \
         --seed 0 \
         --random_weights True 
