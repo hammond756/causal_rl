54c8c4c9ee2dedd3f4d3e742cdea774aacbd7e83
aronhammond <hammond756@live.nl>
2019-07-04 14:25:49 +0200

    added re-implementation of prediction model

--- causal_rl/models/predictors.py
+++ causal_rl/models/predictors.py
@@ -89,7 +89,64 @@ class TwoStepPredictor(nn.Module):
         return prediction
 
 
+class TwoStepMatrixPredictor(nn.Module):
+    def __init__(self, sem):
+        super(TwoStepMatrixPredictor, self).__init__()
+
+        self.dim = sem.dim
+        self.noise = None
+
+        self.abduct = Abductor(self.dim)
+        self.predict = MatrixPredictor(self.dim)
+
+    def forward(self, observation, intervention):
+        noise = self.abduct(observation)
+        self.noise = noise
+        prediction = self.predict(noise, intervention)
+        return prediction
+
+
+class MatrixPredictor(nn.Module):
+    def __init__(self, dim):
+        super(MatrixPredictor, self).__init__()
+        self.dim = dim
+
+        # heuristic: we know the true weights are lower triangular
+        # heuristic: root nodes should have self-connection of 1
+        # to carry noise to prediction
+        self.linear1 = nn.Parameter(
+            torch.randn((self.dim, self.dim)).tril_(-1)
+        )
+
+    def forward(self, noise, intervention):
+        target, value = intervention
+
+        # create dummy vector for 'hetrogeneous coordinates'
+        z = torch.cat([torch.zeros(self.dim), torch.tensor([1.])]).unsqueeze(0)
+        z.requires_grad = False
+
+        # formulate intervention as a vector to be applied to the model matrix
+        do_x = torch.tensor([0. for _ in range(self.dim)] + [value])
+        do_x = do_x.unsqueeze(dim=0)
+        do_x.requires_grad = False
+
+        weights = torch.cat([self.linear1, noise.t()], dim=1)
+
+        model = torch.cat([weights, z], dim=0)
+
+        # perform intervention on the model
+        model[target, :] = do_x
+
+        result = z.clone().t()
+
+        for i in range(self.dim - 1):
+            result = model.matmul(result)
+
+        return result.t()[:, :-1]
+
+
 predictors = {
     'repeated': Predictor,
-    'two_step': TwoStepPredictor
+    'two_step': TwoStepPredictor,
+    'matrix': TwoStepMatrixPredictor
 }
